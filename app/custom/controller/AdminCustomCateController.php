253421aa2007c4041040a3f0857d736a8a92fba5
11223ren <1132974092@qq.com>
2018-10-30 18:30:50 +0800

    cate

---
+++ app/custom/controller/AdminCustomCateController.php
@@ -0,0 +1,276 @@
+<?php
+ 
+namespace app\custom\controller;
+
+ 
+use app\common\controller\AdminInfo0Controller; 
+use think\Db; 
+  
+class AdminCustomCateController extends AdminInfo0Controller
+{
+    
+    public function _initialize()
+    {
+        parent::_initialize();
+       
+        $this->flag='客户类别';
+        $this->table='custom_cate';
+        $this->m=Db::name('custom_cate');
+        //没有店铺区分
+        $this->isshop=0;
+        $this->assign('flag',$this->flag);
+        $this->assign('table',$this->table);
+        
+    }
+    /**
+     * 客户类别列表
+     * @adminMenu(
+     *     'name'   => '客户类别列表',
+     *     'parent' => 'custom/AdminIndex/default',
+     *     'display'=> true,
+     *     'hasView'=> true,
+     *     'order'  => 2,
+     *     'icon'   => '',
+     *     'remark' => '客户类别列表',
+     *     'param'  => ''
+     * )
+     */
+    public function index()
+    {
+         parent::index();
+        return $this->fetch();
+    }
+     
+   
+    /**
+     * 客户类别添加
+     * @adminMenu(
+     *     'name'   => '客户类别添加',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别添加',
+     *     'param'  => ''
+     * )
+     */
+    public function add()
+    {
+        parent::add();
+        return $this->fetch();  
+        
+    }
+    /**
+     * 客户类别添加do
+     * @adminMenu(
+     *     'name'   => '客户类别添加do',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别添加do',
+     *     'param'  => ''
+     * )
+     */
+    public function add_do()
+    {
+        parent::add_do();
+        
+    }
+    /**
+     * 客户类别详情
+     * @adminMenu(
+     *     'name'   => '客户类别详情',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别详情',
+     *     'param'  => ''
+     * )
+     */
+    public function edit()
+    {
+        parent::edit();  
+        return $this->fetch();  
+    }
+    /**
+     * 客户类别状态审核
+     * @adminMenu(
+     *     'name'   => '客户类别状态审核',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别状态审核',
+     *     'param'  => ''
+     * )
+     */
+    public function review()
+    {
+        parent::review();
+    }
+    /**
+     * 客户类别状态批量同意
+     * @adminMenu(
+     *     'name'   => '客户类别状态批量同意',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别状态批量同意',
+     *     'param'  => ''
+     * )
+     */
+    public function review_all()
+    {
+        parent::review_all();
+    }
+    /**
+     * 客户类别禁用
+     * @adminMenu(
+     *     'name'   => '信息状态禁用',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '信息状态禁用',
+     *     'param'  => ''
+     * )
+     */
+    public function ban()
+    {
+        parent::ban();
+    }
+    /**
+     * 客户类别信息状态恢复
+     * @adminMenu(
+     *     'name'   => '客户类别信息状态恢复',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别信息状态恢复',
+     *     'param'  => ''
+     * )
+     */
+    public function cancel_ban()
+    {
+        parent::cancel_ban();
+    }
+    /**
+     * 客户类别编辑提交
+     * @adminMenu(
+     *     'name'   => '客户类别编辑提交',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别编辑提交',
+     *     'param'  => ''
+     * )
+     */
+    public function edit_do()
+    {
+        parent::edit_do();
+    }
+    /**
+     * 客户类别编辑列表
+     * @adminMenu(
+     *     'name'   => '客户类别编辑列表',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别编辑列表',
+     *     'param'  => ''
+     * )
+     */
+    public function edit_list(){
+        parent::edit_list();
+        return $this->fetch();  
+    }
+    
+    /**
+     * 客户类别审核详情
+     * @adminMenu(
+     *     'name'   => '客户类别审核详情',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> true,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别审核详情',
+     *     'param'  => ''
+     * )
+     */
+    public function edit_info()
+    {
+        parent::edit_info();
+        return $this->fetch();  
+    }
+    /**
+     * 客户类别信息编辑审核
+     * @adminMenu(
+     *     'name'   => '客户类别编辑审核',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别编辑审核',
+     *     'param'  => ''
+     * )
+     */
+    public function edit_review()
+    {
+        parent::edit_review();
+    }
+    /**
+     * 客户类别编辑记录批量删除
+     * @adminMenu(
+     *     'name'   => '客户类别编辑记录批量删除',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别编辑记录批量删除',
+     *     'param'  => ''
+     * )
+     */
+    public function edit_del_all()
+    {
+        parent::edit_del_all();
+    }
+    
+    /**
+     * 客户类别批量删除
+     * @adminMenu(
+     *     'name'   => '客户类别批量删除',
+     *     'parent' => 'index',
+     *     'display'=> false,
+     *     'hasView'=> false,
+     *     'order'  => 10,
+     *     'icon'   => '',
+     *     'remark' => '客户类别批量删除',
+     *     'param'  => ''
+     * )
+     */
+    public function del_all()
+    {
+         
+        parent::del_all();
+    }
+   
+     
+}
